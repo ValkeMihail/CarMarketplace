27e032215d14d991abed64e8311dc409b1c7f04e
Mihail Nicoară <mihail.ncr@gmail.com>
2023-07-08 11:40:47 +0300

    added PWA configuration

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import * as serviceWorker from './serviceWorker';
+
+serviceWorker.register();
+
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
