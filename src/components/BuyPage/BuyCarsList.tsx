5a66b2fadb43d70f014896557aa58efdb482de52
Mihail Nicoară <mihail.ncr@gmail.com>
2023-07-30 18:00:37 +0300

    refactored BuyCarList Component, fixed errors

--- src/components/BuyPage/BuyCarsList.tsx
+++ src/components/BuyPage/BuyCarsList.tsx
@@ -1,175 +1,186 @@
-import { collection,db ,getDocs, limit, orderBy, query, where, collectionGroup,OrderByDirection, WhereFilterOp, startAfter} from "../../firebase";
-import {useState,useEffect} from "react";
+import {
+  collection,
+  db,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+  collectionGroup,
+  OrderByDirection,
+  QuerySnapshot,
+  WhereFilterOp,
+  startAfter,
+  QueryDocumentSnapshot,
+  DocumentData,
+  Query,
+} from "../../firebase";
+import { useState, useEffect } from "react";
 import { CarCard } from "../Navigation/CarCard";
-import {filterType,SearchProps, sortType} from "./buy";
+import { filterType, SearchProps, sortType } from "./buy";
 import { LoadingOverlay } from "../Navigation/LoadingOverlay";
 import { showToolTip } from "../Navigation/Footer";
-
+import { Car } from "../../../types";
 
 type AllCarsListProps = {
-   
-    sort  : sortType | null;
-    filter : filterType | null;
-    search : SearchProps | null;
-    startAfterRef : any;
- 
+  sort: sortType | null;
+  filter: filterType | null;
+  search: SearchProps | null;
+  startAfterRef: [] | number | null;
 };
 
-/**
- * Used in BuyPage to display all car ads in the database 
- * @returns  CarCard component with all car ads in the database
- */
-const BuyCarsList = ({ sort, filter ,search, startAfterRef} : AllCarsListProps) => {
-      
-    
-    const [carDocs, setCarDocs] = useState<any[]>([]);
-    const [lastDoc, setLastDoc] = useState<any>(null);
-    const [requestMoreData, setRequestMoreData] = useState<boolean>(false);
-    const [initialLoad, setInitialLoad] = useState<boolean>(true);
-    const [snapSize, setSnapSize] = useState<number>(0);
-    
-    
-    
-    useEffect(() => {
-        setCarDocs([]);
-        setLastDoc(null);
-        setInitialLoad(true);
-      
-       
-          setRequestMoreData(true);
-        
-      }, [filter, sort, search, startAfterRef]);
+const BuyCarsList = ({
+  sort,
+  filter,
+  search,
+  startAfterRef,
+}: AllCarsListProps) => {
+  const [carDocs, setCarDocs] = useState<DocumentData[]>([]);
+  const [lastDoc, setLastDoc] = useState<QueryDocumentSnapshot<DocumentData> | null>(null);
+  const [requestMoreData, setRequestMoreData] = useState<boolean>(false);
+  const [initialLoad, setInitialLoad] = useState<boolean>(true);
+  const [snapSize, setSnapSize] = useState<number>(0);
 
+  useEffect(() => {
+    setCarDocs([]);
+    setLastDoc(null);
+    setInitialLoad(true);
+    setRequestMoreData(true);
+  }, [filter, sort, search, startAfterRef]);
 
-    useEffect(() => {   
-        
-        if (initialLoad == false && requestMoreData == false) {return;}
-        if (snapSize == 0 && initialLoad == false) {return;}
-       
+  useEffect(() => {
+    if (initialLoad == false && requestMoreData == false) return;
+    if (snapSize == 0 && initialLoad == false) return;
 
-        const getCars = async ( sort:sortType | null ,filter :filterType | null , search : SearchProps | null) => {
+    const getCars = async (
+      sort: sortType | null,
+      filter: filterType | null,
+      search: SearchProps | null
+    ) => {
+      const carsQuery = getCarsQuery(sort, filter, search);
 
-                const carsCollection = collection(db, "cars");
-                const carsCollectionGroup = collectionGroup(db, "cars");
-                const carsQuery = 
-                    // if filter and search active 
-                    search != null &&
-                    filter != null  ?    
-                    query(
-                        carsCollectionGroup,
-                        orderBy(sort!.sortProperty , sort!.sortOrder as OrderByDirection),
-                        where(filter!.filterProperty as string, filter!.filterOperator as WhereFilterOp, Number(filter!.filterValue)),
-                        where ("searchKeywords", "array-contains", search!.searchKeyword),
-                        startAfter(lastDoc || startAfterRef),
-                        limit(2)
-                    ):
-                    // if filter inactive and search active
-                    search !=null &&
-                    filter == null ?
-                    query(
-                        carsCollectionGroup,
-                        orderBy(sort!.sortProperty , sort!.sortOrder as OrderByDirection),
-                        where ("searchKeywords", "array-contains", search!.searchKeyword),
-                        startAfter(lastDoc || startAfterRef),
-                        limit(2)
-                    ):
-                    // if filter active and search inactive
-                    search ==null &&
-                    filter != null ?
-                    query(
-                        carsCollection,
-                        orderBy(sort!.sortProperty , sort!.sortOrder as OrderByDirection),
-                        where(filter.filterProperty as string, filter.filterOperator as WhereFilterOp, Number(filter.filterValue)),
-                        startAfter(lastDoc || startAfterRef),
-                        limit(2)
-                    ):
-                    // if filter and search inactive
-                    search ==null &&
-                    filter == null ?
-                    query(
-                        carsCollection,
-                        orderBy(sort!.sortProperty, sort!.sortOrder as OrderByDirection),
-                        startAfter(lastDoc || startAfterRef),
-                        limit(2)
-                    ) :
-                    // if only sort
-                    query(
-                        carsCollection,
-                        orderBy("createdAt", "desc"),
-                        startAfter(lastDoc || startAfterRef),
-                        limit(2)
-                    );
-                
-                const carsSnapshot = await getDocs(carsQuery!);
-                const carsData = carsSnapshot.docs.map(async (CarDoc) => {
-                    const carData = CarDoc.data();
-                    const userID = carData.userID;
-                    carData.id = CarDoc.id;
-                    const userPhotoURL = await getUserPhoto(userID);
-                    carData.userPhoto = userPhotoURL;
-                    return carData;
-                });
-                const carsWithData = await Promise.all(carsData);
-                setLastDoc(carsSnapshot.docs[carsSnapshot.docs.length - 1]);
-                setSnapSize(carsSnapshot.docs.length);
+      const carsSnapshot = await getDocs(carsQuery);
+      const carsWithData = await getCarsData(carsSnapshot);
 
-                if(lastDoc == null ){
-                    setCarDocs(carsWithData);
-                    setInitialLoad(false);
-                }else{
-                    setCarDocs([...carDocs, ...carsWithData]);
-                }
+      setLastDoc(
+        carsSnapshot.docs[
+          carsSnapshot.docs.length - 1
+        ] as QueryDocumentSnapshot<Car>
+      );
+      setSnapSize(carsSnapshot.docs.length);
 
-             
-                
-               
-        };
-        
-        getCars(sort,filter,search);
-        setRequestMoreData(false);
-        
-    }, [requestMoreData]);
-    
-
-
-    /**
-     *  Used to get the user photo url as a string from the users collection
-     * @param userID  The user ID as a string
-     * @returns  The user photo URL as a string
-     */
-    const getUserPhoto = async (userID: string) => {
-        const userDoc = await getDocs(collection(db, "users"));
-        const userDocs = userDoc.docs;
-        const user = userDocs.find((userDoc) => userDoc.id === userID);
-        return user?.data().photoURL as string;
+      if (!lastDoc) {
+        setCarDocs(carsWithData as Car[]);
+        setInitialLoad(false);
+      } else {
+        setCarDocs([...carDocs, ...carsWithData]);
+      }
     };
-  
-    return (
-        <>
-            <CarCard carsDataArray={carDocs}/>
-            {   requestMoreData==false ?(
-                    <button 
-                        onClick={(snapSize == 0) && initialLoad==false ?
-                            () => showToolTip("No more ads", "red")
-                            :() => setRequestMoreData(true)
-                        }
-                        className=
-                        {
-                            snapSize==0 && initialLoad==false
-                             ? "no-button" : "btn btn-social loadMoreBtn"
-                        }
-                        >
-                            { snapSize==0  && initialLoad==false 
-                            ? "No Ads" : "Load More"}
-                    </button>  
-                ):
-                (
-                    <LoadingOverlay/>
-                )    
-            } 
-        </>
-    );
-};
 
+    getCars(sort, filter, search);
+    setRequestMoreData(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [requestMoreData]);
+
+  const getUserPhoto = async (userID: string) => {
+    const userDoc = await getDocs(collection(db, "users"));
+    const userDocs = userDoc.docs;
+    const user = userDocs.find((userDoc) => userDoc.id === userID);
+    return user?.data().photoURL as string;
+  };
+
+  const getCarsData = async (carsSnapshot: QuerySnapshot<DocumentData>) => {
+    const carsData = carsSnapshot.docs.map(async (CarDoc) => {
+      const carData = CarDoc.data();
+      const userID = carData.userID;
+      carData.id = CarDoc.id;
+      const userPhotoURL = await getUserPhoto(userID);
+      carData.userPhoto = userPhotoURL;
+      return carData;
+    });
+
+    return await Promise.all(carsData);
+  };
+
+  const getCarsQuery = (  sort: sortType | null,  filter: filterType | null,  search: SearchProps | null) => {
+    const carsCollection = collection(db, "cars");
+    const carsCollectionGroup = collectionGroup(db, "cars");
+
+    let carsQuery: Query<DocumentData>;
+
+    if (search && filter && sort)
+      carsQuery = query(
+        carsCollectionGroup,
+        orderBy(sort.sortProperty, sort.sortOrder as OrderByDirection),
+        where(
+          filter.filterProperty as string,
+          filter.filterOperator as WhereFilterOp,
+          Number(filter.filterValue)
+        ),
+        where("searchKeywords", "array-contains", search.searchKeyword),
+        startAfter(lastDoc || startAfterRef),
+        limit(2)
+      );
+    else if (search && !filter && sort)
+      carsQuery = query(
+        carsCollectionGroup,
+        orderBy(sort.sortProperty, sort.sortOrder as OrderByDirection),
+        where("searchKeywords", "array-contains", search.searchKeyword),
+        startAfter(lastDoc || startAfterRef),
+        limit(2)
+      );
+    else if (!search && filter && sort)
+      carsQuery = query(
+        carsCollection,
+        orderBy(sort.sortProperty, sort.sortOrder as OrderByDirection),
+        where(
+          filter.filterProperty as string,
+          filter.filterOperator as WhereFilterOp,
+          Number(filter.filterValue)
+        ),
+        startAfter(lastDoc || startAfterRef),
+        limit(2)
+      );
+    else if (!search && !filter && sort)
+      carsQuery = query(
+        carsCollection,
+        orderBy(sort.sortProperty, sort.sortOrder as OrderByDirection),
+        startAfter(lastDoc || startAfterRef),
+        limit(2)
+      );
+    else
+      carsQuery = query(
+        carsCollection,
+        orderBy("createdAt", "desc"),
+        startAfter(lastDoc || startAfterRef),
+        limit(2)
+      );
+    return carsQuery;
+  };
+
+  return (
+    <>
+      <CarCard carsDataArray={carDocs} />
+      {requestMoreData == false ? (
+        <button
+          onClick={
+            snapSize == 0 && initialLoad == false
+              ? () => showToolTip("No more ads", "red")
+              : () => setRequestMoreData(true)
+          }
+          className={
+            snapSize == 0 && initialLoad == false
+              ? "no-button"
+              : "btn btn-social loadMoreBtn"
+          }
+        >
+          {snapSize == 0 && initialLoad == false ? "No Ads" : "Load More"}
+        </button>
+      ) : (
+        <LoadingOverlay />
+      )}
+    </>
+  );
+};
 
 export default BuyCarsList;
--- src/components/BuyPage/buy.tsx
+++ src/components/BuyPage/buy.tsx
@@ -1,13 +1,9 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faClose,
-  faSliders,
-  faSortAsc,
-  faSortDesc,
-  faSearch,
-} from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
 import BuyCarsList from "./BuyCarsList";
+import { FilterWrapContainer } from "../../containers/buyPageContainers/FilterWrapContainer";
+import { SortWrapContainer } from "../../containers/buyPageContainers/SortWrapContainer";
+import { SearchWrapContainer } from "../../containers/buyPageContainers/SearchWrapContainer";
+import { FilterMenu } from "../../containers/buyPageContainers/FilterMenu";
 
 export type sortType = {
   sortProperty: string;
@@ -31,9 +27,9 @@ export function Buy() {
     sortOrder: "desc",
   });
   const [activeSortOrderAsc, setActiveSortOrderAsc] = useState<boolean>(false);
-  const [CurrentStartAfterRef, setCurrentStartAfterRef] = useState<[] | number | null>(
-    null
-  );
+  const [CurrentStartAfterRef, setCurrentStartAfterRef] = useState<
+    [] | number | null
+  >(null);
   const [isFilterActive, setIsFilterActive] = useState<boolean>(false);
   const [filterToQuery, setFilterToQuery] = useState<filterType | null>(null);
   const [filter, setFilter] = useState<filterType | null>({
@@ -91,6 +87,13 @@ export function Buy() {
 
   const changeFilter = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const filterBy = e.target.value;
+    const sortPropertyMap: { [key: string]: string } = {
+      null: "createdAt",
+      price: "price",
+      madeYear: "madeYear",
+      milage: "milage",
+    };
+
     setFilter({
       ...(filter
         ? filter
@@ -101,16 +104,13 @@ export function Buy() {
           }),
       filterProperty: filterBy,
     });
+
     setIsFilterActive(false);
+
     if (filterBy === "null") {
       setFilterToQuery(null);
-      setSort({ ...sort, sortProperty: "createdAt" });
-    } else if (filterBy === "price") {
-      setSort({ ...sort, sortProperty: "price" });
-    } else if (filterBy === "madeYear") {
-      setSort({ ...sort, sortProperty: "madeYear" });
-    } else if (filterBy === "milage") {
-      setSort({ ...sort, sortProperty: "milage" });
+    } else {
+      setSort({ ...sort, sortProperty: sortPropertyMap[filterBy] });
     }
   };
 
@@ -123,156 +123,29 @@ export function Buy() {
     <div className="container">
       <div className="searchBar">
         <div className="filters">
-          <div className="filterWrap">
-            <FontAwesomeIcon icon={faSliders} className="fontAwesomeIcon" />
-
-            <select
-              name="filterBy"
-              className="selectWrapper"
-              onChange={changeFilter}
-              defaultValue="null"
-              id="filterBy"
-            >
-              <option id="null" value="null">
-                No Filters
-              </option>
-              <option id="priceFilter" value="price">
-                Price
-              </option>
-              <option id="yearFilter" value="madeYear">
-                Year
-              </option>
-              <option id="milageFilter" value="milage">
-                Milage
-              </option>
-            </select>
-          </div>
-          <div className="filterWrap">
-            <select
-              name="sortBy"
-              className="selectWrapper"
-              onChange={(e) =>
-                setSort({ ...sort, sortProperty: e.target.value })
-              }
-              id="sortBy"
-            >
-              {filter?.filterProperty === "null" ? (
-                <>
-                  <option value="createdAt">Recently Added</option>
-                  <option value="price">Price</option>
-                  <option value="milage">Milage</option>
-                  <option value="madeYear">Year</option>
-                </>
-              ) :(
-                <option value={filter?.filterProperty}></option>
-              )}
-            </select>
-            <div className="ascdesccontainer">
-              <FontAwesomeIcon
-                onClick={() => setOrder()}
-                icon={faSortAsc}
-                className={
-                  activeSortOrderAsc
-                    ? "fontAwesomeIconBigger"
-                    : "fontAwesomeActive"
-                }
-              />
-              <FontAwesomeIcon
-                onClick={() => setOrder()}
-                icon={faSortDesc}
-                className={
-                  activeSortOrderAsc
-                    ? "fontAwesomeActive"
-                    : "fontAwesomeIconBigger"
-                }
-              />
-            </div>
-          </div>
+          <FilterWrapContainer changeFilter={changeFilter} />
+          <SortWrapContainer
+            setOrder={setOrder}
+            activeSortOrderAsc={activeSortOrderAsc}
+            filter={filter}
+            sort={sort}
+            setSort={setSort}
+          />
         </div>
-        <div className="searchWrap">
-          <div className="form-input">
-            <input
-              onChange={(e) =>
-                setSearch({ ...search, searchKeyword: e.target.value })
-              }
-              id="searchWord"
-              type="text"
-              placeholder="Search"
-            />
-            <FontAwesomeIcon
-              onClick={isSearchActive ? closeSearch : applySearch}
-              icon={isSearchActive ? faClose : faSearch}
-              className="fontAwesomeIconSearch"
-            />
-          </div>
-        </div>
-
-        {filter?.filterProperty === "null" ? null : (
-          <div id="searchBarFilterMenu" className="searchBarFilterMenu">
-            {isFilterActive == true ? (
-              <div id="appliedFilters">
-                <div className="filter">
-                  <h4 id="apliedFilterFor">{filter?.filterProperty}</h4>
-                  <h4 id="apliedFilterOperator">{filter?.filterOperator}</h4>
-                  <h4 id="apliedFilterValue">{filter?.filterValue}</h4>
-                  <h3 id="closeFilter" onClick={closeFilter}>
-                    x
-                  </h3>
-                </div>
-              </div>
-            ) : (
-              <div className="form-input">
-                <label htmlFor={filter?.filterProperty}>
-                  {filter?.filterProperty}
-                </label>
-                <div className="selectWrapper">
-                  <select
-                    className="selectWrapper"
-                    onChange={(e) =>
-                      setFilter({
-                        ...(filter
-                          ? filter
-                          : {
-                              filterProperty: "price",
-                              filterValue: "1000",
-                              filterOperator: ">",
-                            }),
-                        filterOperator: e.target.value,
-                      })
-                    }
-                    defaultValue={filter?.filterOperator}
-                  >
-                    <option value=">">&gt;</option>
-                    <option value="<">&lt;</option>
-                  </select>
-                </div>
-                <input
-                  id="priceToFilter"
-                  onChange={(e) =>
-                    setFilter({
-                      ...(filter
-                        ? filter
-                        : {
-                            filterProperty: "price",
-                            filterValue: "1000",
-                            filterOperator: ">",
-                          }),
-                      filterValue: e.target.value,
-                    })
-                  }
-                  type="text"
-                  placeholder="Enter value"
-                />
-                <button
-                  onClick={applyFilter}
-                  className="btn btn-social applyFilter"
-                >
-                  Apply Filter
-                </button>
-              </div>
-            )}
-          </div>
-        )}
+        <SearchWrapContainer
+          applySearch={applySearch}
+          closeSearch={closeSearch}
+          isSearchActive={isSearchActive}
+          search={search}
+          setSearch={setSearch}
+        />
+        <FilterMenu
+          applyFilter={applyFilter}
+          closeFilter={closeFilter}
+          setFilter={setFilter}
+          filter={filter}
+          isFilterActive={isFilterActive}
+        />
       </div>
       <div className="buyScreen">
         <BuyCarsList
